97a01245bd60e78dd6fc68c099dc14fed9bf03f2
ArlindKryeziu <arlinndkryeziu@gmail.com>
2018-11-17 13:59:06 +0100

    Comments for "decrypt funcion" on aes.py files

--- aes.py
+++ aes.py
@@ -3,9 +3,8 @@ from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto import Random
 
-
 def encrypt(key, source, encode=True):
-    key = SHA256.new(key).digest() #fixed size
+    key = SHA256.new(key).digest() #fixed size key
     iv_1 = Random.new().read(AES.block_size) #initialize vector
     encryptor = AES.new(key, AES.MODE_CBC, iv_1) #encrypt mode
     padding = AES.block_size - len(source) % AES.block_size #padding of the last block
@@ -13,15 +12,14 @@ def encrypt(key, source, encode=True):
     data = iv_1 + encryptor.encrypt(source) #encryption
     return base64.b64encode(data).decode("latin-1") if encode else data #return of the encryted text
 
-
 def decrypt(key, source, decode=True):
     try:
         if decode:
             source = base64.b64decode(source.encode("latin-1"))
-        key = SHA256.new(key).digest()
-        iv_1 = source[:AES.block_size]
+        key = SHA256.new(key).digest() #fixed size key
+        iv_1 = source[:AES.block_size] #initialize vector
         decryptor = AES.new(key, AES.MODE_CBC, iv_1)
-        data = decryptor.decrypt(source[AES.block_size:])
+        data = decryptor.decrypt(source[AES.block_size:]) #decrypted data
         padding = ord(data[-1])
         if data[-padding:] != chr(padding) * padding:
             raise ValueError("Invalid padding...")
